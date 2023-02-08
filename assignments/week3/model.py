726174c41630cd5305c13bdecff76ef3adb81479
Zhiyu Lei <zlei6@seas.upenn.edu>
2023-02-08 01:01:09 -0500

    Submit week3

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -2,7 +2,10 @@ import torch
 from typing import Callable
 
 
-class MLP:
+class MLP(torch.nn.Module):
+    """
+    A multilayer perceptron model
+    """
     def __init__(
         self,
         input_size: int,
@@ -19,12 +22,21 @@ class MLP:
             input_size: The dimension D of the input data.
             hidden_size: The number of neurons H in the hidden layer.
             num_classes: The number of classes C.
+            hidden_count: The number of hidden layers.
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+        self.activation = activation()
+        self.hidden_layers = [torch.nn.Linear(input_size, hidden_size)]
+        initializer(self.hidden_layers[0].weight)
+        for _ in range(1, hidden_count):
+            self.hidden_layers.append(torch.nn.Linear(hidden_size, hidden_size))
+            initializer(self.hidden_layers[-1].weight)
+        self.output_layer = torch.nn.Linear(hidden_size, num_classes)
+        initializer(self.output_layer.weight)
 
-    def forward(self, x):
+    def forward(self, x: torch.tensor) -> torch.tensor:
         """
         Forward pass of the network.
 
@@ -34,4 +46,6 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        for hidden_layer in self.hidden_layers:
+            x = self.activation(hidden_layer(x))
+        return self.output_layer(x)
