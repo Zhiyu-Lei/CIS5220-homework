1d42bea750a5a4e2b563b4ab45c653c26138b11f
Zhiyu Lei <zlei6@seas.upenn.edu>
2023-02-21 17:00:14 -0500

    Try again

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -7,12 +7,12 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 class CONFIG:
     batch_size = 128
-    num_epochs = 5
-    initial_learning_rate = 0.006
+    num_epochs = 20
+    initial_learning_rate = 0.002
     initial_weight_decay = 0.001
 
     lrs_kwargs = {
-        "init_step_size": 391,
+        "init_step_size": 1564,
         "step_size_inc": 2,
         "gamma": 0.5,
     }
